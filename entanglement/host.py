dafe4acaf4ff5fbdec88a7ac7e0607b7f9c2b003
Daniel <rodrigues.iif@gmail.com>
2020-06-14 19:01:13 -0300

    entanglement example

---
+++ entanglement/host.py
@@ -0,0 +1,13 @@
+import qsharp
+
+from qsharp import Result
+from Quantum.Bell import TestSuperpositionState
+from Quantum.Bell import TestBellState
+
+initials = (Result.Zero, Result.One)
+
+for i in initials:
+  #res = TestSuperpositionState.simulate(count=1000, initial=i)
+  res = TestBellState.simulate(count=1000, initial=i)
+  (num_zeros, num_ones, agree) = res
+  print(f'Init:{i: <4} 0s={num_zeros: <4} 1s={num_ones: <4} agree={agree: <4}')
